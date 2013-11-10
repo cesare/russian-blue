149d4796cf3f41cb392ba5348f437e10d3742391
SAWADA Tadashi <cesare@mayverse.jp>
2013-11-10 23:03:24 +0900

    Initial Commit

---
+++ RussianBlue/RussianBlue.h
@@ -0,0 +1,13 @@
+//
+//  RussianBlue.h
+//  RussianBlue
+//
+//  Created by cesare on 2013/11/10.
+//  Copyright (c) 2013年 cesare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RussianBlue : NSObject
+
+@end
